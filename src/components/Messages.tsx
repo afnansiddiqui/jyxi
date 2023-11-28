d4dbc2c2cdb49a111cdd571b0916ce63abfed62f
Afnan Siddiqui <afnan.sid0@gmail.com>
2023-11-29 02:11:29 +0500

    removed

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -3,7 +3,6 @@
 import { pusherClient } from '@/lib/pusher'
 import { cn, toPusherKey } from '@/lib/utils'
 import { Message } from '@/lib/validations/message'
-import { format } from 'date-fns'
 import Image from 'next/image'
 import { FC, useEffect, useRef, useState } from 'react'
 
@@ -25,9 +24,7 @@ const Messages: FC<MessagesProps> = ({
   const [messages, setMessages] = useState<Message[]>(initialMessages)
 
   useEffect(() => {
-    pusherClient.subscribe(
-      toPusherKey(`chat:${chatId}`)
-    )
+    pusherClient.subscribe(toPusherKey(`chat:${chatId}`))
 
     const messageHandler = (message: Message) => {
       setMessages((prev) => [message, ...prev])
@@ -36,9 +33,7 @@ const Messages: FC<MessagesProps> = ({
     pusherClient.bind('incoming-message', messageHandler)
 
     return () => {
-      pusherClient.unsubscribe(
-        toPusherKey(`chat:${chatId}`)
-      )
+      pusherClient.unsubscribe(toPusherKey(`chat:${chatId}`))
       pusherClient.unbind('incoming-message', messageHandler)
     }
   }, [chatId])
@@ -46,7 +41,10 @@ const Messages: FC<MessagesProps> = ({
   const scrollDownRef = useRef<HTMLDivElement | null>(null)
 
   const formatTimestamp = (timestamp: number) => {
-    return format(timestamp, 'HH:mm')
+    const date = new Date(timestamp)
+    const hours = date.getHours().toString().padStart(2, '0')
+    const minutes = date.getMinutes().toString().padStart(2, '0')
+    return `${hours}:${minutes}`
   }
 
   return (
