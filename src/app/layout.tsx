efc7a02034f48d83736b247dc25ab0fc2bb77a89
Afnan Siddiqui <afnan.sid0@gmail.com>
2023-11-28 21:17:35 +0500

    Favicon done now its time for deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,11 @@ import './globals.css'
 export const metadata = {
   title: 'Jyxi | Home',
   description: 'Welcome to the Jyxi',
+  icons:{
+    icon:['/favicon.ico?v=4'],
+    apple:['/apple-touch-icon.png?v=4'],
+    shortcut:['/apple-touch-icon.png']
+  }
 }
 
 export default function RootLayout({
