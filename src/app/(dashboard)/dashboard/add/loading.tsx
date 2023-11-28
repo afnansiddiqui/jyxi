a85f5ee3d76dba05499b19f6f3581b6aa1a07785
Afnan Siddiqui <afnan.sid0@gmail.com>
2023-11-28 21:00:32 +0500

    Did everytthing just loading problem is comming

--- src/app/(dashboard)/dashboard/add/loading.tsx
+++ src/app/(dashboard)/dashboard/add/loading.tsx
@@ -6,8 +6,8 @@ interface loadingProps {}
 
 const loading: FC<loadingProps> = ({}) => {
   return (
-    <div className='w-full flex flex-col gap-3 text-black'>
-      <Skeleton className='mb-4' height={60} width={500} />
+    <div className='w-full flex flex-col gap-3 text-black bg-black'>
+      <Skeleton className='mb-4 text-black bg-black' height={60} width={500} />
       <Skeleton height={20} width={150} />
       <Skeleton height={50} width={400} />
     </div>
